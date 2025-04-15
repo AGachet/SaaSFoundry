fe23ceecf13026da640f03cedfdce186317d422e
AnthoGac <anthony.gachet@gmail.com>
2025-04-15 18:17:15 +0200

    feat(#10): add email common service logic in backend blueprints

--- scaffolds/blueprints/api/src/app.module.ts
+++ scaffolds/blueprints/api/src/app.module.ts
@@ -8,12 +8,12 @@ import { APP_FILTER } from '@nestjs/core'
  * Dependencies
  */
 import { GlobalExceptionFilter } from '@common/filters/global-exception.filter'
-import { HealthModule } from '@modules/health/health.module'
-import { PrismaModule } from '@configs/prisma/prisma.module'
-import { AuthModule } from './modules/auth/auth.module'
-import { EnvModule } from '@configs/env/env.module'
 import { LoggerModule } from '@common/services/logger/logger.module'
+import { EnvModule } from '@configs/env/env.module'
+import { PrismaModule } from '@configs/prisma/prisma.module'
+import { HealthModule } from '@modules/health/health.module'
 import { ApiDocsModule } from './modules/api-docs/api-docs.module'
+import { AuthModule } from './modules/auth/auth.module'
 
 /**
  * Declaration
